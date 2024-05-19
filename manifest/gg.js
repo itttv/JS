9e55114a937c41c3af3890fcd6cec084e6bf1029
itttv <informationtech.ttv@gmail.com>
2024-05-19 16:41:01 +0700

    Update gg.js

--- manifest/gg.js
+++ manifest/gg.js
@@ -51,7 +51,8 @@ function getCookie(_0x52dec3) {
   return _0x9457a0;
 }
 //add iklan
-var _links = ["https://www.highcpmgate.com/ethakbt5rr?key=96da23e9eba28e9a8a2bd7ef2634234b", "https://www.highcpmgate.com/gd8d3cip3k?key=3fac22aa9925d30821e35118852b4550","https://www.highcpmgate.com/tiakwqw2i?key=bb633614a6c98b6c5aeb9913eb3af5fd"];
+var _links = ["https://stoolsymphony.com/tiakwqw2i?key=bb633614a6c98b6c5aeb9913eb3af5fd","https://www.highcpmgate.com/ethakbt5rr?key=96da23e9eba28e9a8a2bd7ef2634234b","https://stoolsymphony.com/gd8d3cip3k?key=3fac22aa9925d30821e35118852b4550
+","https://www.highcpmgate.com/gd8d3cip3k?key=3fac22aa9925d30821e35118852b4550","https://stoolsymphony.com/ethakbt5rr?key=96da23e9eba28e9a8a2bd7ef2634234b","https://www.highcpmgate.com/tiakwqw2i?key=bb633614a6c98b6c5aeb9913eb3af5fd"];
 var _directLinkUrl = '';
 var _get = function (_0x3eea8f) {
   if (_0x3eea8f = new RegExp("[?&]" + encodeURIComponent(_0x3eea8f) + "=([^&]*)").exec(location.search)) {
